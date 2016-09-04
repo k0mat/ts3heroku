9a912a6f96b89d21d9a23748479930c9a964880c
k0mat <kow.mateusz@gmail.com>
2016-09-04 10:21:08 +0200

    port fix

--- start.py
+++ start.py
@@ -1,12 +1,19 @@
+#!/usr/bin/python
+
+import sys
 import subprocess
 
-def main():
+def main(port):
 	running = True
 	command = 'ts3/ts3server_minimal_runscript.sh'
+	args = 'default_voice_port=' + port
 	while running:
-		process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+		process = subprocess.Popen(command + ' ' + args, shell=True, stdout=subprocess.PIPE)
 		process.wait()
 		print process.returncode
 
 if __name__ == '__main__':
-	main()
+	if !len(sys.argv) == 2:
+		print 'Please specify port in command line'
+		return
+	main(sys.argv[1])
